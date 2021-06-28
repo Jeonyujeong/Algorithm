357ed6901a748ed1bd459a8235dd0f3db0608049
Jeonyujeong <dbwjd8752@naver.com>
2021-06-28 16:52:19 +0900

    BOJ 1965. 돌 게임2

---
+++ cpp/dp/9656.cpp
@@ -0,0 +1,12 @@
+// BOJ 1965. 돌 게임2
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+
+    scanf("%d", &n);
+    if (n % 2 == 0) printf("SK");
+    else printf("CY");
+}
